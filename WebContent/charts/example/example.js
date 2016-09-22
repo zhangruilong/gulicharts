f15d4d7ed65d43832e3e4b08f7d80bf3e335c010
taolichao <xvhandong@sina.com>
2016-09-22 13:29:45 +0800

    注释

--- WebContent/charts/example/example.js
+++ WebContent/charts/example/example.js
@@ -6,7 +6,9 @@ Knot.ready(function(){
         url:"bookstore.json"
     }).then(function(ret){
             window.greetingModel.categories = [{name:"aaa",name2:"aaa2"},{name:"bbb",name2:"bbb2"},{name:"ccc",name2:"ccc2"},{name:"fff",name2:"fff2"}];
-        },
+
+           
+    },
         function(err){
             alert(err);
         }
